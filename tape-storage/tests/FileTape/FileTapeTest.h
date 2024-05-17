9b40484dcb4f2d641cff00a456d2a05b404bf823
Nomlsbad <iliayurasov@yandex.ru>
2024-05-17 12:29:30 +0300

    Make paths variables const in test

--- tape-storage/tests/FileTape/FileTapeTest.h
+++ tape-storage/tests/FileTape/FileTapeTest.h
@@ -5,9 +5,9 @@ class FileTapeTest: public ::testing::Test
 {
 protected:
 
-    static constexpr std::string testsDir = TESTS_DIR;
-    static constexpr std::string testTapePath = testsDir + "/FileTape/testTape.txt";
-    static constexpr std::string writeTestTapePath = testsDir + "/FileTape/writeTestTape.txt";
+    inline static const std::string testsDir = TESTS_DIR;
+    inline static const std::string testTapePath = testsDir + "/FileTape/testTape.txt";
+    inline static const std::string writeTestTapePath = testsDir + "/FileTape/writeTestTape.txt";
 };
 
 #endif // TAPE_STORAGE_FILETAPE_TEST_H
